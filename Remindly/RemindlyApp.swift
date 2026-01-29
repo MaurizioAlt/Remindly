37af8964ff582dbf2e153dd117b65f8e18231e32
Maurizio Altamura <maurizioaltamura2@gmail.com>
2026-01-29 13:39:15 -0500

    Initial Commit

---
+++ Remindly/RemindlyApp.swift
@@ -0,0 +1,20 @@
+//
+//  RemindlyApp.swift
+//  Remindly
+//
+//  Created by Maurizio Altamura on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct RemindlyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
